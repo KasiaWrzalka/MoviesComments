03d71f86f69a12ec29d7ff46986acc41d7ad6a5b
KasiaWrzalka <katarzyna@wrzalka.com>
2018-10-01 23:10:07 +0200

    Database settings for Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,21 +77,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'netguru',
-        'USER': 'kasia',
-        'PASSWORD': 'kasia123',
-        'HOST': '127.0.0.1',
-        'PORT': '5433',
-    }
-}
+# Heroku settings
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
+#############################################################
+#     Local settings. Uncomment to start local version.     #
+#############################################################
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'netguru',
+#         'USER': 'kasia',
+#         'PASSWORD': 'kasia123',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5433',
 #     }
 # }
 
@@ -137,3 +137,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+#
